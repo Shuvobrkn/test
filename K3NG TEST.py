37820ef945bd7e26dc2f97c84ec2d82bd1ad3340
Shuvobrkn <127725883+Shuvobrkn@users.noreply.github.com>
2023-03-14 01:03:03 +0600

    Create K3NG TEST.py
    pkg update
    pkg upgrade
    pkg install python
    pkg install python2
    pkg install git
    pkg install pip
    pip install requests
    pip install mechanize
    pip install rich
    pip install bs4
    
    git clone https://github.com/Shuvobrkn/test.git
    ls
    cd K3NG RANDOM
    ls
    python K3NG RANDOM.py

---
+++ K3NG TEST.py
@@ -0,0 +1,211 @@
+import os
+
+try:
+
+	import requests
+
+except ImportError:
+
+	print('\ [×] requests module not installed!...\')
+
+	os.system('pip install requests')
+
+try:
+
+	import concurrent.futures
+
+except ImportError:
+
+	print('\ [×] Futures module not installed!...\')
+
+	os.system('pip install futures')
+
+try:
+
+	import bs4
+
+except ImportError:
+
+	print('\ [×] Bs4 module not installed!...\')
+
+	os.system('pip install bs4')
+
+import os
+
+import requests,bs4,json,sys,random,datetime,time,re,subprocess,platform
+
+from bs4 import BeautifulSoup as sop
+
+from concurrent.futures import ThreadPoolExecutor as tred
+
+import zlib
+
+from time import sleep
+
+import os,sys,time,json,random,re,string,platform,base64,platform
+
+try:
+
+	import requests
+
+	from concurrent.futures import ThreadPoolExecutor as ThreadPool
+
+	import mechanize
+
+	from requests.exceptions import ConnectionError
+
+except ModuleNotFoundError:
+
+	os.system('pip install mechanize requests futures==2 > /dev/null')
+
+from bs4 import BeautifulSoup
+
+R = '\1b[1;91m' 
+
+G = '\1b[1;92m' 
+
+Y = '\1b[1;93m' 
+
+try:
+
+	import os,requests,json,time,re,random,sys,uuid,string,subprocess
+
+	from string import *
+
+	import bs4
+
+	#import dz
+
+	from concurrent.futures import ThreadPoolExecutor as tred
+
+	from bs4 import BeautifulSoup as sop
+
+	from bs4 import BeautifulSoup
+
+except ModuleNotFoundError: 
+
+	print('\ Installing missing modules ...')
+
+	os.system('pip install requests bs4 futures==2 > /dev/null')
+
+	os.system('python Zohaib.py')
+
+ugen = []
+
+for xd in range(5000):
+
+	aa='Mozilla/5.0 (Linux; U; Android'
+
+	b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
+
+	c=' en-us; GT-'
+
+	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+
+	e=random.randrange(1, 999)
+
+	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+
+	g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+
+	h=random.randrange(73,100)
+
+	i='0'
+
+	j=random.randrange(4200,4900)
+
+	k=random.randrange(40,150)
+
+	l='Mobile Safari/537.36'
+
+	uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+
+	ugen.append(uaku2)
+
+try:
+
+	prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+
+	open('.prox.txt','w').write(prox)
+
+except Exception as e:
+
+	print('\1b[1;95m[√] LOADING...')
+
+	os.system('clear')
+
+	
+
+for xd in range(10000):
+
+	a='Mozilla/5.0 (Symbian/3; Series60/'
+
+	b=random.randrange(1, 9)
+
+	c=random.randrange(1, 9)
+
+	d='Nokia'
+
+	e=random.randrange(100, 9999)
+
+	f='/110.021.0028; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/535.1 (KHTML, like Gecko) NokiaBrowser/'
+
+	g=random.randrange(1, 9)
+
+	h=random.randrange(1, 4)
+
+	i=random.randrange(1, 4)
+
+	j=random.randrange(1, 4)
+
+	k='Mobile Safari/535.1'
+
+	uaku=(f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}.{j} {k}')
+
+	ugen.append(uaku)
+
+	aa='Mozilla/5.0 (Linux; U; Android'
+
+	b=random.choice(['6','7','8','9','10','11','12'])
+
+	c=' en-us; GT-'
+
+	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+
+	e=random.randrange(1, 999)
+
+	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+
+	g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+
+	h=random.randrange(73,100)
+
+	i='0'
+
+	j=random.randrange(4200,4900)
+
+	k=random.randrange(40,150)
+
+	l='Mobile Safari/537.36'
+
+	uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
+
+	ugen.append(uaku2)
+
+for xd in range(3000):
+
+	build_nokiax = ['JDQ39','JZO54K']
+
+	rr = random.randint; rc = random.choice
+
+	miui_v3 = ['-g','-gn','-go','-gn','gzip(gfe)',' swan-mibrowser']
+
+	miui_v1 = ['0','1','2','3','4','5','6','7','8','9','10','11','12']
+
+	miui_v2 = ['0','1','2','3','4','5','6','7','8','9','10','11','14','22','27','36']
+
+	aZ = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+	basa = ['en-us','en-gb','id-id','de-de','ru-ru','en-sg','fr-fr','fa-ir','ja-jp','pt-br','cs-cz','zh-hk','zh-cn','vi-vn','en-ph','en-in','tr-tr']
+
+	gt = ['GT-1015','GT-1020','GT-1030','GT-1035','GT-1040','GT-1045','GT-1050','GT-1240','GT-1440','GT-1450','GT-18190','GT-18262','GT-19060I','GT-19082','GT-19083','GT-19105','GT-19152','GT-19192','GT-19300','GT-19505','GT-2000','GT-20000','GT-200s','GT-3000','GT-414XOP','GT-6918','GT-7010','GT-7020','GT-7030','GT-7040','GT-7050','GT-7100','GT-7105','GT-7110','GT-7205','GT-7210','GT-7240R','GT-7245','GT-7303','GT-7310','GT-7320','GT-7325','GT-7326','GT-7340','GT-7405','GT-7550	5GT-8005','GT-8010','GT-81','GT-810','GT-8105','GT-8110','GT-8220S','GT-8410','GT-9300','GT-9320','GT-93G','GT-A7100','GT-A9500','GT-ANDROID','GT-B2710','GT-B5330','GT-B5330B','GT-B5330L','GT-B5330ZKAINU','GT-B5510','GT-B5512','GT-B5722','GT-B7510','GT-B7722','GT-B7810','GT-B9150','GT-B9388','GT-C3010','GT-C3262','GT-C3310R','GT-C
